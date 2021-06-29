1680899f1be72b7c52e451d6a9bd507ad5712fb2
Tagore Athikela <tagoreathikela123@gmail.com>
2021-06-29 22:21:43 +0530

    Update and rename assignment1@29-06-2021 to assignment1@29-06-2021.java

--- assignment1@29-06-2021.java
+++ assignment1@29-06-2021.java
@@ -1,5 +1,8 @@
 //Program to print multiplication of given number
 
+import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Solution {
     public static void main(String[] args) throws IOException {
