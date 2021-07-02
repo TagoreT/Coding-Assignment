0e3c37985643888d4cae48ee52de5983228984c9
Tagore Athikela <tagoreathikela123@gmail.com>
2021-07-02 19:35:47 +0530

    Create assignment3@02-07-2021.java

---
+++ assignment3@02-07-2021.java
@@ -0,0 +1,23 @@
+//Program to print *
+
+import java.io.*;
+import java.util.*;
+
+public class asign3 {
+    public static void main(String[] args){  
+
+        int n;
+        Scanner sc = new Scanner(System.in);  
+    
+        System.out.print("Enter the number : ");
+        n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++){ 
+            for (int j = 1; j <= i; j++){  
+                System.out.print(j);  
+            }   
+            System.out.println("");
+        }
+                
+    }
+}
