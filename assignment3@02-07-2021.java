764c8de532d908a02ad3829ae21521b1b344a7d8
Tagore Athikela <tagoreathikela123@gmail.com>
2021-07-02 19:38:01 +0530

    Update assignment3@02-07-2021.java

--- assignment3@02-07-2021.java
+++ assignment3@02-07-2021.java
@@ -14,7 +14,7 @@ public class asign3 {
 
         for (int i = 1; i <= n; i++){ 
             for (int j = 1; j <= i; j++){  
-                System.out.print(j);  
+                System.out.print(j + " ");  
             }   
             System.out.println("");
         }
